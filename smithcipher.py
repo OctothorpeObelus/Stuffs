7ab7c613d38f3d69e7b4ac930f67da2d519d7d62
Octo <OctothorpeObelus@users.noreply.github.com>
2019-03-27 14:58:03 -0400

    Added decode function to smithcipher.py

--- smithcipher.py
+++ smithcipher.py
@@ -10,3 +10,18 @@ def encode(message): #123456789
   for i in range(len(msgout)):
     msgout[i] = chr(ord(msgout[i]) + 3)
   return "".join(msgout) #;975468:<
+
+def decode(message):
+  msg=list(message)
+  msgout=[]
+  msg1=(msg[::-1])[math.floor(len(message)/2):]
+  msg2=message[math.floor(len(message)/2):]
+  for i in range(len(msg)):
+    if (i%2==0):
+      msgout.insert(i,msg2[math.floor(i/2)])
+    elif (i%2!=0):
+      msgout.insert(i,msg1[math.floor(i/2)])
+  
+  for i in range(len(msgout)):
+    msgout[i] = chr(ord(msgout[i]) - 3)
+  return "".join(msgout)
