7308fa2de91ab7c256c4434e4cb4a74abd9ab46f
Octo <OctothorpeObelus@users.noreply.github.com>
2019-03-27 14:22:39 -0400

    Initial completion of smithcipher.pi
    An attempt was made

--- smithcipher.py
+++ smithcipher.py
@@ -1,1 +1,9 @@
-
+def encode(message): #123456789
+  msg=list(message) #Split message into characters
+  msgout=[] #Instantiate encoded message
+  for i in range(len(msg)):
+    if (i%2==0):
+      msgout.append(msg[i])
+    else:
+      msgout.insert(0,msg[i])
+  return "".join(msgout) #864213579
