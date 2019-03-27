a9d82270680842e87ac74ce1ee99447a1afd9ade
Octo <OctothorpeObelus@users.noreply.github.com>
2019-03-27 14:30:28 -0400

    Update smithcipher.py
    Hints

--- smithcipher.py
+++ smithcipher.py
@@ -1,4 +1,4 @@
-def encode(message):
+def encode(message): #123456789
   msg=list(message) #Split message into characters
   msgout=[] #Instantiate encoded message
   for i in range(len(msg)):
@@ -6,6 +6,7 @@ def encode(message):
       msgout.append(msg[i])
     else:
       msgout.insert(0,msg[i])
+  #864213579
   for i in range(len(msgout)):
     msgout[i] = chr(ord(msgout[i]) + 3)
-  return "".join(msgout)
+  return "".join(msgout) #;975468:<
